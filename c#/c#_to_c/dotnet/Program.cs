09102f7022e641843d87e98d66d0a38939dc527c
Scott Ketelaar <git@scottketelaar.com>
2017-09-29 20:52:17 -0400

    Add dotnet

---
+++ c#/c#_to_c/dotnet/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace dotnet {
+	class Program {
+		// Import our shared functions here
+		[DllImport ("libnative")]
+		public static extern IntPtr say_hello();
+
+		[DllImport ("libnative")]
+		public static extern int multiply(int x, int y);
+
+		[DllImport ("libnative")]
+		public static extern int min(int[] array, int len);
+
+		static void Main(string[] args) {
+			var hello_ptr = say_hello();
+			var hello = Marshal.PtrToStringAnsi(hello_ptr);
+
+			Console.WriteLine(hello);
+
+			var result = multiply(4, 5);
+
+			Console.WriteLine("4 x 5 = " + result);
+
+			int[] arr = { 5, 2, 9, 5, 99 };
+			var minimum = min(arr, arr.Length);
+
+			Console.WriteLine("Min of array is " + minimum);
+
+			Console.WriteLine("Press any key to exit . . .");
+			Console.ReadKey();
+		}
+	}
+}
