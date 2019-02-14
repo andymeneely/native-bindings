00141fa19643d6a7718def0d773c5b833f698986
George Herde <herde.george@gmail.com>
2019-02-14 11:56:07 -0500

    Update lab3.c

--- python_to_c/lab3.c
+++ python_to_c/lab3.c
@@ -3,14 +3,9 @@
 
 // For Python Bindings use: 
 // gcc -shared -o lab3.so -fPIC lab3.c
-// python3 cPython.py
+// python3 main.py
 
 int multiply(int, int);
-
-char[] helloworld() {
-	char str[] = "Hello, World!";
-	return str;
-}
 
 int multiply(int num1, int num2) {
     return num1 * num2;
